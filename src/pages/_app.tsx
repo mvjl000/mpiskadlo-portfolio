ffc1aa2fe35a87a66f8d31d86d7afb216e45e5a1
Miłosz <milosz.devmail@gmail.com>
2023-02-10 10:53:23 +0100

    Setup tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Unbounded, Anton, Montserrat } from "@next/font/google";
+
+const unbounded = Unbounded({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  variable: "--font-unbounded",
+});
+const anton = Anton({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-anton",
+});
+const montserrat = Montserrat({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main
+        className={`${montserrat.variable} ${unbounded.variable} ${anton.variable} font-sans`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)', ...fontFamily.sans],
+        unbounded: ['var(--font-unbounded)', ...fontFamily.sans],
+        anton: ['var(--font-anton)', ...fontFamily.sans],
+      },
+      colors: {
+				'hero-bg': '#131313',
+				'default-font': '#ffffff',
+      }
+    },
+  },
+  plugins: [],
+}
