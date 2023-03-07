25501b7e4ed725163b538ac17a02f6a2f488092b
Miłosz <milosz.devmail@gmail.com>
2023-03-07 14:46:02 +0100

    Add mq breakpoints

--- src/components/Hero/DownButton.tsx
+++ src/components/Hero/DownButton.tsx
@@ -39,71 +39,71 @@ export const DownButton = () => {
       <motion.button
         type="button"
         onClick={handleScroll}
-        className="block w-[200px] h-[200px] border-2 border-white rounded-full hover:bg-slate-900 transition-colors relative cursor-pointer"
+        className="block w-[150px] h-[150px] md:w-[200px] md:h-[200px] border-[1px] md:border-2 border-white rounded-full hover:bg-slate-900 transition-colors relative cursor-pointer"
         whileHover={{ scale: 1.1 }}
       >
-        <p className="text-white font-mono text-xl origin-center">
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-80deg]">
+        <p className="text-white font-mono text-base md:text-xl origin-center">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-80deg]">
             G
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-72deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-72deg]">
             e
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-64deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-64deg]">
             t
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-56deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-56deg]">
             {" "}
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-48deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-48deg]">
             t
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-40deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-40deg]">
             o
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-32deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-32deg]">
             {" "}
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-24deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-24deg]">
             k
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-16deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-16deg]">
             n
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-8deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[-8deg]">
             o
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[0deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[0deg]">
             w
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[8deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[8deg]">
             {" "}
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[16deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[16deg]">
             m
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[24deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[24deg]">
             e
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[32deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[32deg]">
             {" "}
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[40deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[40deg]">
             b
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[48deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[48deg]">
             e
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[56deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[56deg]">
             t
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[64deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[64deg]">
             t
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[72deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[72deg]">
             e
           </span>
-          <span className="h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[80deg]">
+          <span className="h-[75px] md:h-[100px] absolute top-0 left-1/2 translate-x-[-50%] origin-bottom rotate-[80deg]">
             r
           </span>
         </p>
