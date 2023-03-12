3a06b17da92b3cf6d51da4fde1536af431cd62d7
Miłosz <milosz.devmail@gmail.com>
2023-03-12 23:31:27 +0100

    Create separate navigation button component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,11 @@
 import { AnimatePresence, motion, Variants } from "framer-motion";
-import { useOpenMenu } from "@/hooks";
 import { CloseBurger } from "@/components/Navigation/Burger";
 import { Panels } from "@/components/Navigation/Panels";
 import { isMenuOpenAtom } from "@/store";
 import { useAtom } from "jotai";
+import { NavigationButton } from "./NavigationButton";
 
-type Section = "experience" | "projects" | "skills";
-
-const menuVariants: Variants = {
+const variants: Variants = {
   initial: {
     visibility: "hidden",
   },
@@ -21,31 +19,8 @@ const menuVariants: Variants = {
   },
 };
 
-const buttonsVariants: Variants = {
-  exit: {
-    visibility: "hidden",
-    transition: { delay: 0.5 },
-  },
-};
-
 export const Navigation = () => {
   const [isMenuOpen] = useAtom(isMenuOpenAtom);
-  const { handleToggleMenu } = useOpenMenu();
-
-  const handleScroll = (section: Section) => {
-    const selectedSection = document.getElementById(section);
-    if (!selectedSection) return;
-
-    const clickedButton = document.activeElement as HTMLButtonElement;
-
-    if (clickedButton) {
-      // prevents animations overlaping
-      clickedButton.blur();
-    }
-
-    selectedSection.scrollIntoView();
-    handleToggleMenu();
-  };
 
   return (
     <AnimatePresence>
@@ -53,7 +28,7 @@ export const Navigation = () => {
         <>
           <motion.nav
             className="fixed inset-0 flex justify-center items-center z-40 overflow-hidden bg-black"
-            variants={menuVariants}
+            variants={variants}
             initial="initial"
             animate="animate"
             exit="exit"
@@ -67,37 +42,13 @@ export const Navigation = () => {
             </motion.div>
             <ul className="flex flex-col gap-5">
               <li>
-                <motion.button
-                  value="Experience"
-                  className="font-unbounded text-yellow-400 text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl uppercase relative after:content-[attr(value)] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-0 after:text-black after:bg-yellow-400 after:overflow-hidden after:w-full after:h-0 hover:after:h-full focus-visible:after:h-full after:transition-all after:duration-[450ms] after:ease-in-out"
-                  onClick={() => handleScroll("experience")}
-                  variants={buttonsVariants}
-                  exit="exit"
-                >
-                  Experience
-                </motion.button>
+                <NavigationButton sectionName="experience" />
               </li>
               <li className="border-y-2 py-5 border-stone-900">
-                <motion.button
-                  value="Projects"
-                  className="font-unbounded text-yellow-400 text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl uppercase relative after:content-[attr(value)] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-0 after:text-black after:bg-yellow-400 after:overflow-hidden after:w-full after:h-0 hover:after:h-full focus-visible:after:h-full  after:transition-all after:duration-[450ms] after:ease-in-out"
-                  onClick={() => handleScroll("projects")}
-                  variants={buttonsVariants}
-                  exit="exit"
-                >
-                  Projects
-                </motion.button>
+                <NavigationButton sectionName="projects" />
               </li>
               <li>
-                <motion.button
-                  value="Skills"
-                  className="font-unbounded text-yellow-400 text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl uppercase relative after:content-[attr(value)] after:absolute after:top-1/2 after:-translate-y-1/2 after:left-0 after:text-black after:bg-yellow-400 after:overflow-hidden after:w-full after:h-0 hover:after:h-full focus-visible:after:h-full  after:transition-all after:duration-[450ms] after:ease-in-out"
-                  onClick={() => handleScroll("skills")}
-                  variants={buttonsVariants}
-                  exit="exit"
-                >
-                  Skills
-                </motion.button>
+                <NavigationButton sectionName="skills" />
               </li>
             </ul>
           </motion.nav>
